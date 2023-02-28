2fa2af397d184af1dbb17f7d490b43657b7c0a35
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-02-28 19:11:01 -0300

    Avançando no curso JavaScrip

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Analizando nacionalidade...... Seu país: ${pais}.`)
+if (pais == 'Brasil') {
+    console.log('Brasileiro.')
+}else {
+    console.log('Estrangeiro.')
+}
