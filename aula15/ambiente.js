6274c3b3acdc63afae9ace4a62c26124039c14d5
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2023-03-28 18:46:49 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+var num = [5,8,9,2,4,3]
+console.log(`Nosso vetor é o: ${num}`)
+num[6]=0 //adiciona um valor no índice 6 da variável
+console.log(`Nosso vetor é o: ${num}`)
+num.push(7) //adiciona um número no final do array
+console.log(`Nosso vetor é o: ${num}`)
+// num.length comprimento do vetor
+num.sort() //coloca os elementos em órdem crescente
+console.log(`Nosso vetor é o: ${num}`)
+console.log(`O vetor tem ${num.length} valores.`)
